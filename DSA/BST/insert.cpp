09be64dc87a9ef266bd3cf7eb56437ed8b05f2bf
asyncShaurya <shauryaofficial96@gmail.com>
2025-07-18 21:05:33 +0530

    done modification

--- DSA/BST/insert.cpp
+++ DSA/BST/insert.cpp
@@ -92,6 +92,67 @@ Node* insertIntoBST(Node* root, int d){
      return root;
 }
 
+Node* minVal(Node* root){
+    Node* temp = root;
+    while(temp->left != NULL){
+        temp = temp->left;  
+    }
+    return temp;
+}
+
+Node* maxVal(Node* root){
+    Node* temp = root;
+    while(temp->right != NULL){
+        temp = temp->right;  
+    }
+    return temp;
+}
+
+Node* deleteFromBST(Node* root, int val){
+     if(root == NULL){
+        return root;
+     }
+
+     if(root->data == val){
+         //0 child
+         if(root->left == NULL && root->right == NULL){
+            delete root;
+            return NULL;
+         }
+         //1 child
+
+         //left case
+         if(root->left != NULL && root->right == NULL){
+              Node* temp = root->left;
+              delete root;
+              return temp;
+         }
+
+         //right child
+         if(root->left == NULL && root->right != NULL){
+            Node* temp = root->right;
+            delete root;
+            return temp;
+         }
+
+         //2 child
+         if(root->left != NULL && root->right != NULL){
+            int mini = minVal(root->right) -> data;
+            root -> data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+         }
+     }
+     else if(root->data > val){
+        root->left = deleteFromBST(root->left, val);
+        return root;
+     }
+     else{
+        root->right = deleteFromBST(root->right, val);
+        return root;
+     }
+}
+
 void takeInput(Node* &root){
     int data;
     cin >> data;
@@ -121,5 +182,10 @@ int main(){
     postorder(root);
     cout << endl;
 
+    cout << "Min value is " << minVal(root)-> data << endl;
+    cout << "Max value is " << maxVal(root)-> data << endl;
+    
+    root = deleteFromBST(root, 30);
+    
     return 0;
 }
