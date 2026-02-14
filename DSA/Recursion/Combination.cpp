e2c9a304fb7d67a54ec1f598c849e8b840f0ccc1
asyncShaurya <shauryaofficial96@gmail.com>
2026-02-14 23:26:20 +0530

    Last Bit

---
+++ DSA/Recursion/Combination.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int fact (int n){
+    if(n==0){
+        return 1;
+    }
+    return fact(n-1)*n;
+}
+int nCr(int n, int r){
+    int num,den;
+    num = fact(n);
+    den = fact(r)*fact(n-r);
+
+    return num/den;
+}
+//Parscal Triange Meth.
+int NCR(int n, int r){
+    if(n==r || r==0){
+        return 1;
+    }
+    return NCR(n-1,r-1)+NCR(n-1,r);
+}
+int main(){
+    cout << NCR(4,2) << endl;
+    return 0;
+}
