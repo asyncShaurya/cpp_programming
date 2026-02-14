44cf2e0303eec8e344a37698745c0fb0b6da933d
asyncShaurya <shauryaofficial96@gmail.com>
2026-02-14 23:08:49 +0530

    Done with Recursion

---
+++ DSA/Recursion/Taylor_Series.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+//This is a simple way of writing  
+// double e(int x, int n){
+//     static double p=1, f=1;
+//     double r;
+//     if(n==0){
+//         return 1;
+//     }
+//     r=e(x, n-1);
+//     p=p*x;
+//     f=f*n;
+//     return r+p/f;
+// }
+
+//This is the Horner's Rule 
+// double e(int x, int n){
+//     static double s;
+//     if(n==0){
+//         return s;
+//     }
+//     s=1+x*s/n;
+//     return e(x,n-1);
+// }
+
+//This is the iterative version
+double e(int x, int n){
+    double s=1;
+    int i;
+    double num =1;
+    double den =1;
+
+    for(i=1;i<=n;i++){
+        num *=x;
+        den *=i;
+        s += num/den;
+    }
+    return s;
+}
+
+int main(){
+    cout << e(1,10) << endl;
+    return 0;
+}
